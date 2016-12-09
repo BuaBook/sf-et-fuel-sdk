76fd1fd4d9d007c3b9e171713ca7870d193e95a6
Jaskirat Rajasansir <jas@buabook.com>
2016-12-09 12:37:08 +0000

    Add new ExactTarget interface (from internal project)
     - readme: Add basic description of ExactTargetInterface.java
     - pom: Add extra configuration for JavaDoc plugin

--- README.md
+++ README.md
@@ -13,8 +13,16 @@ To-do:
 
 ### `AutoRefreshETClient`
 
-We've found that the default `ETClient` class does not support refreshing of the OAuth refresh token. This token is defined by the OAuth standard to expire after 14 days at which point connection to ExactTarget will be lost.
+We've found that the default `ETClient` class does not support refreshing of the OAuth refresh token. This token is defined by the OAuth standard to expire after 14 days after which point all API requests will fail.
 
 This class extends `ETClient` and provides automatic refreshing of the refresh token after a duration which can be configured by the caller (there is a default of refreshing every 24 hours if none is provided). The rest of the class behaves identically to `ETClient`.
 
 Use the static method `AutoRefreshETClient.newRefreshClient` to get a new object.
+
+### `ExactTargetInterface`
+
+This class provides some interface methods to retrieving / sending data via the ExactTarget API.
+
+Available methods:
+
+* `getDataExtension`: Retrieves a data extension by using the name as appears on the ExactTarget web UI 
---
+++ src/main/java/com/buabook/exacttarget/fuelsdk/ExactTargetInterface.java
@@ -0,0 +1,55 @@
+package com.buabook.exacttarget.fuelsdk;
+
+import java.util.logging.Logger;
+
+import com.exacttarget.fuelsdk.ETClient;
+import com.exacttarget.fuelsdk.ETDataExtension;
+import com.exacttarget.fuelsdk.ETFilter;
+import com.exacttarget.fuelsdk.ETSdkException;
+
+/**
+ * <h3>ExactTarget API Interface</h3>
+ * <p>Simplified interface to querying ExactTarget via FueldSDK</p>
+ * <p>(c) 2016 Sport Trades Ltd</p>
+ * <p>License GPLv3</p>
+ * 
+ * @author Jas Rajasansir
+ * @version 1.0.0
+ * @since 8 Dec 2016
+ */
+public class ExactTargetInterface {
+	private static final Logger log = Logger.getLogger(ExactTargetInterface.class.getName());
+
+	
+	private final ETClient client;
+	
+	
+	public ExactTargetInterface(ETClient client) throws IllegalArgumentException {
+		if(client == null)
+			throw new IllegalArgumentException("ETClient must be provided");
+		
+		this.client = client;
+		
+		log.info("ExactTarget interface initialised [ Access Token: " + client.getAccessToken() + " ]");
+	}
+	
+	/**
+	 * Provides access to a data extension, querying by the data extension's name (as shown on the ExactTarget web interface)
+	 * @param extensionName The name of the data extension to query
+	 * @return The data extension
+	 * @throws IllegalArgumentException If the name supplied is <code>null</code> or empty string
+	 */
+	public ETDataExtension getDataExtension(String extensionName) throws IllegalArgumentException, ETSdkException {
+		if(extensionName == null || extensionName.isEmpty())
+			throw new IllegalArgumentException("Empty extension name is not permitted");
+		
+		String parseString = "Name=" + extensionName;
+
+		log.fine("Attempting to retrieve Data Extension from ExactTarget [ Name: " + parseString + " ]");
+		
+		ETFilter filter = ETFilter.parse(parseString);
+		
+		return client.retrieveObject(ETDataExtension.class, filter);
+	}
+
+}
